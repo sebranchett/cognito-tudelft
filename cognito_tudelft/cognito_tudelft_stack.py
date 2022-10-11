fa0bc79cd9c0eb025055812b4e429d8b06716a4b
sebranchett <s.e.branchett@tudelft.nl>
2022-10-11 12:27:00 +0200

    add user pool client

--- cognito_tudelft/cognito_tudelft_stack.py
+++ cognito_tudelft/cognito_tudelft_stack.py
@@ -1,6 +1,7 @@
 from aws_cdk import (
     Stack, RemovalPolicy, CfnOutput,
     aws_cognito as cognito,
+    custom_resources as cr,
 )
 from constructs import Construct
 
@@ -10,17 +11,76 @@ class CognitoTudelftStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # User pool and user pool OAuth client
+        # TODO these variables will become input parameters
         base_name = "CogTUD"
+        domain_name = "my-service.my-domain.nl"
+        domain_prefix = "my-service-secure"
+
+        # User pool and user pool OAuth client
         cognito_user_pool = cognito.UserPool(
             self,
             f'{base_name}UserPool',
             removal_policy=RemovalPolicy.DESTROY,
             self_sign_up_enabled=False
         )
+
         # Output Cognito user pool id for future reference
         CfnOutput(
             self,
             f'{base_name}UserPoolID',
             value=cognito_user_pool.user_pool_id
+        )
+
+        cognito_app_client = cognito.UserPoolClient(
+            self,
+            f'{base_name}UserPoolClient',
+            user_pool=cognito_user_pool,
+            generate_secret=True,
+            supported_identity_providers=[
+                cognito.UserPoolClientIdentityProvider.COGNITO],
+            prevent_user_existence_errors=True,
+            o_auth=cognito.OAuthSettings(
+                callback_urls=[
+                    'https://' + domain_name +
+                    '/hub/oauth_callback'
+                ],
+                flows=cognito.OAuthFlows(
+                    authorization_code_grant=True,
+                    implicit_code_grant=True
+                ),
+                scopes=[cognito.OAuthScope.PROFILE, cognito.OAuthScope.OPENID]
+            )
+        )
+
+        describe_cognito_user_pool_client = cr.AwsCustomResource(
+            self,
+            f'{base_name}UserPoolClientIDResource',
+            policy=cr.AwsCustomResourcePolicy.from_sdk_calls(
+                resources=cr.AwsCustomResourcePolicy.ANY_RESOURCE),
+            on_create=cr.AwsSdkCall(
+                service='CognitoIdentityServiceProvider',
+                action='describeUserPoolClient',
+                parameters={
+                    'UserPoolId': cognito_user_pool.user_pool_id,
+                    'ClientId': cognito_app_client.user_pool_client_id
+                },
+                physical_resource_id=cr.PhysicalResourceId.of(
+                    cognito_app_client.user_pool_client_id)
+            )
+        )
+
+        # cognito_user_pool_client_secret = \
+        describe_cognito_user_pool_client.get_response_field(
+            'UserPoolClient.ClientSecret'
+        )
+
+        # domain_prefix=application_prefix + '-' + suffix
+        # cognito_user_pool_domain = \
+        cognito.UserPoolDomain(
+            self,
+            f'{base_name}UserPoolDomain',
+            cognito_domain=cognito.CognitoDomainOptions(
+                domain_prefix=domain_prefix
+            ),
+            user_pool=cognito_user_pool
         )
