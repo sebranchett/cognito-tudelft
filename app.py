b1b7f94f56486981cc01346005a307b821f6b874
sebranchett <s.e.branchett@tudelft.nl>
2022-10-14 14:28:47 +0200

    added lambda function to remove @ and . from username

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import aws_cdk as cdk
 
 from cognito_tudelft.cognito_tudelft_stack import CognitoTudelftStack
 
+# These environment variables set based on your default AWS profile or
+# the profile you specify the --profile option
 default_env = cdk.Environment(
     account=os.getenv('CDK_DEFAULT_ACCOUNT'),
     region=os.getenv('CDK_DEFAULT_REGION')
