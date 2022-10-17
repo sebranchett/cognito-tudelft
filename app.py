c0de8324d25f996728057c32c547b554b3231dd9
sebranchett <s.e.branchett@tudelft.nl>
2022-10-17 12:35:17 +0200

    rearrange for cleaner future use

--- app.py
+++ app.py
@@ -1,9 +1,40 @@
 #!/usr/bin/env python3
 import os
-
 import aws_cdk as cdk
+from aws_cdk import (
+    Stack, RemovalPolicy,
+    aws_cognito as cognito,
+)
+from constructs import Construct
+from cognito_tudelft.tudelft_idp import configure_user_pool
 
-from cognito_tudelft.cognito_tudelft_stack import CognitoTudelftStack
+
+class CognitoTudelftStack(Stack):
+
+    def __init__(
+        self, scope: Construct, construct_id: str,
+        **kwargs
+    ) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        base_name = "TU-Delft"
+        domain_name = "my-service.my-domain.nl"
+
+        cognito_user_pool = cognito.UserPool(
+            self,
+            f'{base_name}UserPool',
+            removal_policy=RemovalPolicy.DESTROY,
+            self_sign_up_enabled=False
+        )
+
+        # cognito_app_client = \
+        configure_user_pool(
+            self,
+            base_name=base_name,
+            domain_name=domain_name,
+            cognito_user_pool=cognito_user_pool
+        )
+
 
 # These environment variables set based on your default AWS profile or
 # the profile you specify the --profile option
