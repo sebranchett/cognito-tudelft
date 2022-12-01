4e26331c718d9da855e11ccfc025615afee1fe8d
sebranchett <s.e.branchett@tudelft.nl>
2022-12-01 14:38:02 +0100

    manipulate dummy template file

---
+++ .github/workflows/convert.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# substitute the UserPoolId in a CloudFormation template with the text
+# "<my_user_pool_id_here>", to make the template easier to reuse
+import json
+
+with open('./cdk.out/CognitoTudelftStack.template.json') as f:
+    data = json.load(f)
+
+for resource in data["Resources"]:
+    if "UserPoolId" in data["Resources"][resource]["Properties"]:
+        data["Resources"][resource]["Properties"]["UserPoolId"] = \
+            "<my_user_pool_id_here>"
+        print(data["Resources"][resource]["Properties"]["UserPoolId"])
+
+with open('./cdk.out/Dummy.CognitoTudelftStack.template.json', 'w') as f:
+    json.dump(data, f, ensure_ascii=False, indent=1)
